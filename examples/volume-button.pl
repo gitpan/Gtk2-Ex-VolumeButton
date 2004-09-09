4c04c2b59e0b30b9ad31b916b86ce96d0b18ed61
Florian Ragwitz <rafl@debian.org>
2004-09-09 11:44:19 +0000

    Import of FLORA/Gtk2-Ex-VolumeButton-0.02 from CPAN.
    gitpan-cpan-distribution: Gtk2-Ex-VolumeButton
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FLORA/Gtk2-Ex-VolumeButton-0.02.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

---
+++ examples/volume-button.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use Gtk2 -init;
+use Gtk2::Ex::VolumeButton;
+
+my $w = Gtk2::Window->new();
+my $b = Gtk2::VBox->new();
+my $v = Gtk2::Ex::VolumeButton->new(
+		zero_image		=> 'gtk-quit',
+		min_image		=> 'gtk-ok',
+		medium_image	=> 'gtk-quit',
+		max_image		=> 'gtk-ok'
+);
+
+$b->pack_start($v, 0, 0, 0);
+$w->add($b);
+$w->show_all();
+
+Gtk2->main();
